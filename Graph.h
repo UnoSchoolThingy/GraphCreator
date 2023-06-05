97e1846e7498148b1b4e721c99fec46e49553244
U-BSD\463453 <463453@SHS-HJCBND2.beaverton.k12.or.us>
2023-06-05 12:24:21 -0700

    mostly done

--- Graph.h
+++ Graph.h
@@ -6,17 +6,17 @@
 #include <set>
 #include <map>
 
-using std::cout;
+using std::cout; using std::endl;
 
 struct Edge {
   struct Vertex* e1, *e2;
-  float weight;
+  int weight;
 
   Edge() {
     memset(this, 0x0, sizeof(Edge));
   }
 
-  Edge(Vertex* e1, Vertex* e2, float weight) {
+  Edge(Vertex* e1, Vertex* e2, int weight) {
     this->e1 = e1;
     this->e2 = e2;
     this->weight = weight;
@@ -36,17 +36,23 @@ struct Vertex { // basically a node
   }
 
   // Make a new edge and connect it 
-  void addEdge(Vertex* otherEnd, float weight) {
+  void addEdge(Vertex* otherEnd, int weight) {
     Edge* edge = new Edge(this, otherEnd, weight);
     edges.insert(edge);
     otherEnd->edges.insert(edge);
   }
 
-  
+  // Check if connected
+  Edge* getConnectingEdge(Vertex* v) {
+    for (Edge* e : edges) {
+      if (e->getOtherEnd(v) == v) return e;
+    }
+    return nullptr;
+  }
   
   ~Vertex() {
     for (Edge* e : edges) { // Go through all the edges and get rid of them 
-      e->getOtherEnd(this)->deleteEdge(e);
+      e->getOtherEnd(this)->edges.erase(e);
       delete e;
     }
   }
@@ -76,12 +82,14 @@ struct Graph {
     return nullptr;
   }
 
+  
+
   // Add an edge with two vertex labels and weight 
-  bool addEdge(char* v1n, char* v2n, float weight) {
+  bool addEdge(char* v1n, char* v2n, int weight) {
     Vertex* v1 = findVertex(v1n);
     Vertex* v2 = findVertex(v2n);
     if (!v1 || !v2) return false;
-    v1.addEdge(v2, weight);
+    v1->addEdge(v2, weight);
     return true;
   }
 
@@ -90,12 +98,57 @@ struct Graph {
     Vertex* v2 = findVertex(v2n);
     if (!v1 || !v2) return false;
     for (Edge* e : v1->edges) {
-      if (e->getOtherSide(v1) == v2) { // Found edge
-	
+      if (e->getOtherEnd(v1) == v2) { // Found edge
+	v1->edges.erase(e);
+	v2->edges.erase(e);
+	delete e;
 	return true;
       }
     }
     return false;
+  }
+
+  bool removeVertex(char* label) { // Terrible code but I was done by the time I realized so it's staying 
+    Vertex* v = findVertex(label);
+    if (v == nullptr) return false;
+    // Vertex destructor handles edges
+    for (int i = 0; i < vertices.size(); i++) {
+      if (v == vertices.at(i)) {
+	vertices.erase(vertices.begin() + i);
+	delete v;
+	return true;
+      }
+    }
+    return false;
+  }
+  
+  void print() {
+    // Print key 
+    cout << "Key:\n";
+    int size = vertices.size();
+    for (int i = 0; i < size; i++) {
+      cout << (i < 10 ? "00" : "0") << i << " | " << vertices.at(i)->label << endl;
+    }
+    // Print adjacency table 
+    cout << "TABLE:\n";
+    cout << "   |";
+    for (int i = 0; i < size; i++) cout << (i < 10 ? "00" : "0") << i << '|';
+    cout << endl;
+    for (int y = 0; y < size; y++) {
+      cout << (y < 10 ? "00" : "0") << y << '|';
+      for (int x = 0; x < size; x++) {
+	if (x == y) {
+	  cout << "---|";
+	  continue;
+	}
+	Edge* e = vertices.at(x)->getConnectingEdge(vertices.at(y));
+	if (e != nullptr) {
+	  cout << (e->weight < 10 ? "00" : "0") << e->weight << '|';
+	}
+	else cout << "INF|";
+      }
+      cout << endl;
+    }
   }
   
   // Dijkstra thingy 
@@ -123,12 +176,13 @@ struct Graph {
 
       if (u == goal) { // Reached the goal! 
 	cout << "Path found, too insane!\n";
-	// Reconstruct path (will do later) 
+	// Reconstruct path (will do later)
+	return;
       }
 
-      for (Edge& e : u->edges) {
-	float alt = info[u].dist + e.weight;
-	TraversalInfo& vInfo = info[e.dst]; // e.dst is like v from the wikipedia thingy 
+      for (Edge* e : u->edges) {
+	float alt = info[u].dist + e->weight;
+	TraversalInfo& vInfo = info[e->getOtherEnd(u)]; // e.dst is like v from the wikipedia thingy 
 	if (alt < vInfo.dist) {
 	  vInfo.dist = alt;
 	  vInfo.cameFrom = u;
@@ -136,6 +190,6 @@ struct Graph {
       }
       
     }
-    
+    cout << "No path found!\n";
   };
 };
--- main.cpp
+++ main.cpp
@@ -4,14 +4,56 @@
 using namespace std;
 
 int main() {
+  cout << "Hi, ADD/DEL V/E is add/delete edge (ADDV is add vertex)" << endl;
   Graph graph;
-  char in[35];
+  char in[35], in2[35], in3[35];
   while (true) {
-    cout << "Enter command (ADDV, ADDE, DELV, DELE, PRINT): ";
+    cout << "Enter command (ADDV, ADDE, DELV, DELE, PATH, PRINT, QUIT): ";
     cin >> in;
-    if (Utils::chkcmd(in, "addv")) {
-
+    if (Utils::chkcmd(in, "addv")) { // Add a vertex 
+      cout << "Enter vertex name: ";
+      cin.ignore();
+      cin.getline(in2, 35);
+      graph.addVertex(in2);
+      cout << "Added!\n";
     }
+    else if (Utils::chkcmd(in, "adde")) {
+      cout << "Enter vertex 1: ";
+      cin.ignore();
+      cin.getline(in2, 35);
+      cout << "Enter vertex 2: ";
+      cin.getline(in3, 35);
+      int w;
+      cout << "Enter edge weight: ";
+      cin >> w;
+      if (w > 999 || w < 1) cout << "Weight has to be between 1 and 999!\n";
+      else cout << (graph.addEdge(in2, in3, w) ? "Added!" : "Couldn't add!") << endl;
+    }
+    else if (Utils::chkcmd(in, "delv")) {
+      cout << "Enter vertex name: ";
+      cin.ignore();
+      cin.getline(in2, 35);
+      cout << (graph.removeVertex(in2) ? "Removed!" : "No such vertex exists!") << endl;
+    }
+    else if (Utils::chkcmd(in, "dele")) {
+      cin.ignore();
+      cout << "Enter vertex 1: ";
+      cin.getline(in2, 35);
+      cout << "Enter vertex 2: ";
+      cin.getline(in3, 35);
+      cout << (graph.removeEdge(in2, in3) ? "Removed!" : "No such edge exists!") << endl;
+    }
+    else if (Utils::chkcmd(in, "path")) {
+      cin.ignore();
+      cout << "Enter vertex 1: ";
+      cin.getline(in2, 35);
+      cout << "Enter vertex 2: ";
+      cin.getline(in3, 35);
+      graph.findPath(in2, in3);
+    }
+    else if (Utils::chkcmd(in, "print")) graph.print();
+    else if (Utils::chkcmd(in, "quit")) return 0;
+    else cout << "Unrecognized command!\n";
   }
   return 0;
 }
